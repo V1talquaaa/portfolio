d0abdaa1b85094221c6face8c4e3cafb734225fb
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2024-02-02 19:37:56 +0100

    fixed logo in footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer id="footer" className="bg-footerBg bg-cover py-[120px]"> 
-      <div className="container text-white flex justify-between items-center">
+      <div className="container text-white flex justify-between items-center" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
         <div>
           <h2 className="mb-5 text-6xl">Follow me</h2>
           <Image src="/footerRounds.svg" width={150} height={10} alt="" />
@@ -19,7 +19,7 @@ function Footer() {
             <li><a href="https://t.me/v1talquaaa" target="_blank" className=""><Image src="/Telegram.svg" width={36} height={36} alt="social" /></a></li>
             <li><a href="https://www.whatsapp.com/" className=""><Image src="/WhatsApp.svg" width={36} height={36} alt="social" /></a></li>
             <li><a href="https://www.facebook.com/vitaliy.sydorenko.9" target="_blank" className=""><Image src="/Facebook.svg" width={36} height={36} alt="social" /></a></li>
-            <li><a href="https://www.linkedin.com/in/vitalii-sydorenko-11b20aa8/" target="_blank" className=""><Image src="/LinkedinIcon.svg" width={36} height={36} alt="social" /></a></li>
+            <li><a href="https://www.linkedin.com/in/vitalii-sydorenko-11b20aa8/" target="_blank" className=""><Image src="/linkedin.svg" width={36} height={36} alt="linkedin" /></a></li>
             <li><a href="https://github.com/V1talquaaa" target="_blank" className=""><Image src="/Github.svg" width={36} height={36} alt="social" /></a></li>
         </ul>
       </div>
