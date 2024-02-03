78e1c04c5442496523d4f3657489b991ecdd8a01
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2024-02-03 15:08:34 +0100

    Update Stack.jsx

--- src/app/components/Stack.jsx
+++ src/app/components/Stack.jsx
@@ -9,17 +9,17 @@ function Stack() {
 <h2 className="mx-auto lg:ml-0 lg:text-left text-2xl lg:text-6xl">My Stack</h2>
       <Image src="/skillArrowIcon.svg" width={600} height={10} alt='arrow' className='hidden lg:block'/>
 </div>
-      <ul className='grid grid-cols-2 gap-5 justify-center'>
-        <li className='flex items-center gap-5'><Image src="/htmlIcon.svg" width={48} height={48} alt="html" />HTML</li>
-        <li className='flex items-center gap-5'><Image src="/jsIcon.svg" width={48} height={48} alt="js" />JavaScript</li>
-        <li className='flex items-center gap-5'><Image src="/nodeIcon.svg" width={48} height={48} alt="node" />Node.js</li>
-        <li className='flex items-center gap-5'><Image src="/reactIcon.svg" width={48} height={48} alt="react" />React</li>
-        <li className='flex items-center gap-5'><Image src="/sassIcon.svg" width={48} height={48} alt="sass" />SAAS</li>
-        <li className='flex items-center gap-5'><Image src="/cssIcon.svg" width={48} height={48} alt="css" />CSS</li>
-        <li className='flex items-center gap-5'><Image src="/nextIcon.svg" width={48} height={48} alt="next" />Next.js</li>
-        <li className='flex items-center gap-5'><Image src="/bootstrapIcon.svg" width={48} height={48} alt="bootstrap"/>Bootstrap</li>
-        <li className='flex items-center gap-5'><Image src="/viteIcon.svg" alt="vite" width={48} height={48} />Vite</li>
-        <li className='flex items-center gap-5'><Image src="/tailwindIcon.svg" alt="tailind" width={48} height={48} />Tailwind</li>
+      <ul className='grid grid-cols-2 text-sm gap-5 justify-center'>
+        <li className='flex items-center gap-5'><Image src="/htmlIcon.svg" width={36} height={36} alt="html" />HTML</li>
+        <li className='flex items-center gap-5'><Image src="/jsIcon.svg" width={36} height={36} alt="js" />JavaScript</li>
+        <li className='flex items-center gap-5'><Image src="/nodeIcon.svg" width={36} height={36} alt="node" />Node.js</li>
+        <li className='flex items-center gap-5'><Image src="/reactIcon.svg" width={36} height={36} alt="react" />React</li>
+        <li className='flex items-center gap-5'><Image src="/sassIcon.svg" width={36} height={36} alt="sass" />SAAS</li>
+        <li className='flex items-center gap-5'><Image src="/cssIcon.svg" width={36} height={36} alt="css" />CSS</li>
+        <li className='flex items-center gap-5'><Image src="/nextIcon.svg" width={36} height={36} alt="next" />Next.js</li>
+        <li className='flex items-center gap-5'><Image src="/bootstrapIcon.svg" width={36} height={36} alt="bootstrap"/>Bootstrap</li>
+        <li className='flex items-center gap-5'><Image src="/viteIcon.svg" alt="vite" width={36} height={36} />Vite</li>
+        <li className='flex items-center gap-5'><Image src="/tailwindIcon.svg" alt="tailind" width={36} height={36} />Tailwind</li>
       </ul>
       </div>
     </section>
