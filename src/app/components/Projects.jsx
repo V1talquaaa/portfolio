482dfbae6a6b8ae4289c9e5416c5e311bee5a13d
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2024-02-06 20:59:06 +0100

    added description to projects

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
       <div className="container"  data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
         <h2 className="mb-5 lg:mb-[80px] text-2xl lg:text-6xl text-white text-center lg:text-right">Individual Projects</h2>
         
-        <div className="flex items-center mb-10">
+        <div className="flex items-center mb-10 lg:mb-[150px]">
         <a href="https://the-royal-curry.vercel.app/" target="_blank" rel="noopener noreferrer nofollow">
           <div className="mr-10 p-2 rounded-xl customShadow">
             <div className="mr-10 p-2 rounded-xl customShadow">
@@ -24,18 +24,26 @@ function Projects() {
           <div>
             <h3 className="text-white lg:text-xl">Royal Curry</h3>
             <span className="text-gray-600">In progres...</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+            Pet project for indian restaurant with possibility to order food online.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">&#91; React, JavaScript &#93;</p>
           </div>
           <div className="ml-auto">
           <Image src="/verticalDodes.svg" width={68} height={266} alt="svg" className=""/>
           </div>
         </div>
-        <div className="flex items-center justify-end mb-10">
+        <div className="flex items-center justify-end mb-10 lg:mb-[150px]">
         <div className="mr-auto">
           <Image src="/horizontalDodes.svg" width={266} height={723} alt="svg" className=""/>
           </div>
           <div className="">
             <h3 className="text-white text-xl">Startupz</h3>
             <span className="text-gray-600">Dec 2023</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+            Project was developed as a part of recruitment process to the company startupz.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">&#91; React, SASS, JavaScript &#93;</p>
           </div>
           <a href="https://startupz-orcin.vercel.app/" target="_blank">
           <div className="ml-10 p-2 rounded-xl customShadow">
@@ -51,7 +59,7 @@ function Projects() {
           </div>
           </a>
         </div>
-        <div className="flex items-center mb-10">
+        <div className="flex items-center mb-10 lg:mb-[150px]">
         <a href="https://v1talquaaa.github.io/adRespect_JS/" target="_blank">
           <div className="mr-10 p-2 rounded-xl customShadow">
             <div className="mr-10 p-2 rounded-xl customShadow">
@@ -68,19 +76,27 @@ function Projects() {
           <div>
             <h3 className="text-white text-xl">Giard Design</h3>
             <span className="text-gray-600">Jul 2023</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+            Project was developed as a part of recruitment process.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">&#91; React, SASS, JavaScript &#93;</p>
           </div>
           <div className="ml-auto">
           <Image src="/verticalDodes.svg" width={68} height={266} alt="svg" className=""/>
           </div>
         </div>
 
-        <div className="flex items-center justify-end mb-10">
+        <div className="flex items-center justify-end mb-10 lg:mb-[150px]">
         <div className="mr-auto">
           <Image src="/horizontalDodes.svg" width={266} height={723} alt="svg" className=""/>
           </div>
           <div className="">
             <h3 className="text-white text-xl">Film Searcher</h3>
             <span className="text-gray-600">Jun 2023</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+            Pet project was a part of course where I used my knowlage how to work with API and fetch data from API.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">&#91; React, JavaScript, API &#93;</p>
           </div>
           <a href="https://v1talquaaa.github.io/goit-react-hw-05-movies/" target="_blank">
           <div className="ml-10 p-2 rounded-xl customShadow">
@@ -98,7 +114,7 @@ function Projects() {
           
         </div>
         
-        <div className="flex items-center mb-10">
+        <div className="flex items-center mb-10 lg:mb-[150px]">
         <a href="https://v1talquaaa.github.io/Barbershop/" target="_blank">
           <div className="mr-10 p-2 rounded-xl customShadow">
             <div className="mr-10 p-2 rounded-xl customShadow">
@@ -115,6 +131,10 @@ function Projects() {
           <div>
             <h3 className="text-white text-xl">Barbershop</h3>
             <span className="text-gray-600">Feb 2023</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+            Pet project to train and improve marup and style skills.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">&#91; HTML, CSS, JavaScript &#93;</p>
           </div>
           <div className="ml-auto">
           <Image src="/verticalDodes.svg" width={68} height={266} alt="svg" className=""/>
@@ -128,6 +148,10 @@ function Projects() {
           <div className="">
             <h3 className="text-white text-xl">Webstudio</h3>
             <span className="text-gray-600">Dec 2022</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+            Pet project as a part of HTML and CSS course.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">&#91; HTML, CSS, JavaScript &#93;</p>
           </div>
           <a href="https://v1talquaaa.github.io/WebStudio/" target="_blank">
           <div className="ml-10 p-2 rounded-xl customShadow">
--- src/app/components/TeamProjects.jsx
+++ src/app/components/TeamProjects.jsx
@@ -3,14 +3,23 @@ import React from "react";
 
 function TeamProjects() {
   return (
-    <section id="projects" className="bg-projectBg bg-cover py-[60px] lg:py-[120px]">
+    <section
+      id="projects"
+      className="bg-projectBg bg-cover py-[60px] lg:py-[120px]"
+    >
       <div className="container">
-        <h2 className="mb-[80px] text-2xl lg:text-6xl text-white text-center lg:text-right">Team Projects</h2>
-        
-        <div className="flex items-center mb-10">
-        <a href="https://nevada-frontend.vercel.app/" target="_blank" rel="noopener noreferrer nofollow">
-          <div className="mr-10 p-2 rounded-xl customShadow">
+        <h2 className="mb-[80px] text-2xl lg:text-6xl text-white text-center lg:text-right">
+          Team Projects
+        </h2>
+
+        <div className="flex items-center mb-10 lg:mb-[150px]">
+          <a
+            href="https://ppcwarehouses.com/"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
             <div className="mr-10 p-2 rounded-xl customShadow">
+              <div className="mr-10 p-2 rounded-xl customShadow">
                 <Image
                   src="/nevada.png"
                   width={400}
@@ -18,45 +27,72 @@ function TeamProjects() {
                   alt="nevada"
                   className="customShadow"
                 />
+              </div>
             </div>
-          </div>
           </a>
           <div>
             <h3 className="text-white text-xl">PPC Wherehouse</h3>
             <p className="text-white my-2">Role: Developer</p>
-            <span className="text-gray-600">In progres...</span>
+            <span className="text-gray-600">Feb 2024</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+              Commercial Project: Website Developed for a Logistics Company
+              Providing Services in the USA and Canada The website was developed
+              for a company specializing in logistics services across the USA
+              and Canada. With several strategically located warehouses
+              throughout both countries, the company primarily caters to Amazon
+              as its main customer.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">
+              &#91; React, Node, Strapi, MUI, JavaScript &#93;
+            </p>
           </div>
           <div className="ml-auto">
-          <Image src="/verticalDodes.svg" width={68} height={266} alt="svg" className=""/>
+            <Image
+              src="/verticalDodes.svg"
+              width={68}
+              height={266}
+              alt="svg"
+              className=""
+            />
           </div>
         </div>
-        <div className="flex items-center justify-end mb-10">
-        <div className="mr-auto">
-          <Image src="/horizontalDodes.svg" width={266} height={723} alt="svg" className=""/>
+        <div className="flex items-center justify-end mb-10 lg:mb-[150px]">
+          <div className="mr-auto">
+            <Image
+              src="/horizontalDodes.svg"
+              width={266}
+              height={723}
+              alt="svg"
+              className=""
+            />
           </div>
-          <div className="">
+          <div>
             <h3 className="text-white text-xl">Task Pro</h3>
             <p className="text-white my-2">Role: Developer</p>
             <span className="text-gray-600">Oct 2023</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+              Pet project which was completed by team of 6 developers as part of
+              the final examination at GOIT School. Similar to Trello and Jira.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">
+              &#91; React, Node, MUI, JavaScript &#93;
+            </p>
           </div>
           <a href="https://korvan17.github.io/task-pro/" target="_blank">
-          <div className="ml-10 p-2 rounded-xl customShadow">
             <div className="ml-10 p-2 rounded-xl customShadow">
-
-                <Image
-                  src="/taskpro.png"
-                  width={400}
-                  height={300}
-                  alt="film"
-                />
+              <div className="ml-10 p-2 rounded-xl customShadow">
+                <Image src="/taskpro.png" width={400} height={300} alt="film" />
+              </div>
             </div>
-          </div>
           </a>
         </div>
         <div className="flex items-center mb-10">
-        <a href="https://slavikua1996.github.io/team-project13/" target="_blank">
-          <div className="mr-10 p-2 rounded-xl customShadow">
+          <a
+            href="https://slavikua1996.github.io/team-project13/"
+            target="_blank"
+          >
             <div className="mr-10 p-2 rounded-xl customShadow">
+              <div className="mr-10 p-2 rounded-xl customShadow">
                 <Image
                   src="/icecream.png"
                   width={400}
@@ -64,16 +100,29 @@ function TeamProjects() {
                   alt="film"
                   className="customShadow"
                 />
+              </div>
             </div>
-          </div>
           </a>
           <div>
             <h3 className="text-white text-xl">Ice Cream</h3>
             <p className="text-white my-2">Role: Scrum, Developer</p>
             <span className="text-gray-600">Jan 2023</span>
+            <p className="mt-2 text-gray-400 text-sm max-w-[400px]">
+              Pet project built solely with HTML and CSS after completion of the
+              markup and stylization part of the course.
+            </p>
+            <p className="mt-2 text-fuchsia-400 text-md ">
+              &#91; HTML, CSS, SASS, JavaScript &#93;
+            </p>
           </div>
           <div className="ml-auto">
-          <Image src="/verticalDodes.svg" width={68} height={266} alt="svg" className=""/>
+            <Image
+              src="/verticalDodes.svg"
+              width={68}
+              height={266}
+              alt="svg"
+              className=""
+            />
           </div>
         </div>
       </div>
@@ -82,4 +131,3 @@ function TeamProjects() {
 }
 
 export default TeamProjects;
-
